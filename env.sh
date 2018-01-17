bb68e2696095e84f209f5f7c75e7ef19d82f6db3
kyleclo <kyleclo@uw.edu>
2018-01-17 00:16:18 -0800

    switch to miniconda

--- env.sh
+++ env.sh
@@ -5,23 +5,23 @@ CONDAENV=ai2-results-extraction
 if ! (which conda); then
 	echo "No `conda` installation found.  Installing..."
 	if [[ $(uname) == "Darwin" ]]; then
-	  wget --continue http://repo.continuum.io/archive/Anaconda3-4.3.1-MacOSX-x86_64.sh
-	  bash Anaconda3-4.3.1-MacOSX-x86_64.sh -b
+	  wget --continue https://repo.continuum.io/miniconda/Miniconda3-4.3.31-Linux-x86_64.sh
+	  bash Miniconda3-4.3.31-Linux-x86_64.sh -b
 	else
-	  wget --continue http://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
-	  bash Anaconda3-4.3.1-Linux-x86_64.sh -b
+	  wget --continue https://repo.continuum.io/miniconda/Miniconda3-4.3.31-MacOSX-x86_64.sh
+	  bash Miniconda3-4.3.1-MacOSX-x86_64.sh -b
 	fi
 fi
 
-export PATH=$HOME/anaconda3/bin:$PATH
+export PATH=$HOME/miniconda3/bin:$PATH
 
-source ~/anaconda3/bin/deactivate ${CONDAENV}
+source ~/miniconda3/bin/deactivate ${CONDAENV}
 
 conda remove -y --name ${CONDAENV} --all
 
 conda create -n ${CONDAENV} -y python=3.6 pytest || true
 
-echo "Activating Conda Environment ----->"
-source ~/anaconda3/bin/activate ${CONDAENV}
+echo "Activating Miniconda Environment ----->"
+source ~/miniconda3/bin/activate ${CONDAENV}
 
 pip install -r requirements.txt
