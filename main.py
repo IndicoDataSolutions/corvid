9d2f6e283f1f1d3fec893b6e97958eebf72cda30
kyleclo <kyleclo@uw.edu>
2018-03-09 11:58:13 -0800

    main

---
+++ main.py
@@ -0,0 +1,28 @@
+"""
+
+Executes entire table aggregation pipeline
+
+    Input: `datasets.json` containing `paper_id` for each dataset of interest
+    Output: `agg_tables.json` containing aggregated tables per dataset
+
+
+for dataset in datasets:
+    tables = []
+    papers = find_relevant_papers(dataset)
+    for paper in papers:
+        pdf_path = fetch_pdf(paper)
+        xml_path = parse_pdf(pdf_path)
+        tables.add(extract_tables(xml_path))
+
+    normalize(tables)
+    filter_relevant(tables)
+
+    gold_tables = load_gold_tables(dataset)
+    for gold_table in gold_tables:
+        target_schema = gold_table.schema
+        dont_cheat = [table for table in tables if table.paper_id != gold_table.paper_id]
+        pred_table = aggregate_tables(dont_cheat, target_schema)
+        metrics = evaluate_metrics(gold_table, pred_table)
+        save(dataset, pred_table, metrics)
+"""
+
