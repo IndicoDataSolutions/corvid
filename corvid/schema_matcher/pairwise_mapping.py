0e0c0a54fe278ffc66b811213008907ba2c0c2f0
cmkumar87 <cmkumar087@gmail.com>
2018-03-15 10:56:32 -0700

    minor fix from PR review

--- corvid/schema_matcher/pairwise_mapping.py
+++ corvid/schema_matcher/pairwise_mapping.py
@@ -10,10 +10,15 @@ from typing import List, Tuple
 
 class PairwiseMapping(object):
     """
+    Class stores output from a schema matcher between a pair of
+    tables along with references to the two tables.
+    Output includes a list of column mappings between the two tables
+    and a cumulative score indicating how well the the two tables map
     """
 
-    def __init__(self, table1: Table, table2: Table, score: float,
-                 column_mappings: List[Tuple[int, int]]):
+    def __init__(self, table1: Table, table2: Table,
+                 column_mappings: List[Tuple[int, int]] = [],
+                 score: float = 0.0):
         self.table1 = table1
         self.table2 = table2
         self.score = score
