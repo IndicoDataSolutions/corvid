5cf93fc216572ce1a9cf8694b83938d89b125cff
kyleclo <kyleclo@uw.edu>
2018-03-16 14:10:29 -0700

    fix example config.py data directory

--- example_config.py
+++ example_config.py
@@ -15,7 +15,7 @@ ES_DEV_URL = 'http://my.dev.es.server.url:8080/'
 S3_PDFS_URL = 's3://my-s3-bucket-url'
 TET_BIN_PATH = canonicalize_path('/Volumes/TET-5.1-OSX-Perl-PHP-Python-Ruby/bin/tet')
 
-DATA_DIR = canonicalize_path('~/ai2/corvid/data')
+DATA_DIR = canonicalize_path('data/')
 DATASETS_JSON = os.path.join(DATA_DIR, 'datasets.json')
 PDF_DIR = os.path.join(DATA_DIR, 'pdf/')
 TETML_DIR = os.path.join(DATA_DIR, 'tetml/')
