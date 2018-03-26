29b345b2dfccf3c918bb2ec89f56fde6eb519bf3
kyleclo <kyleclo@uw.edu>
2018-03-26 02:12:47 -0700

    omnipage pdf parser; call to binary

--- corvid/pipeline/pdf_parser.py
+++ corvid/pipeline/pdf_parser.py
@@ -20,7 +20,6 @@ class OmnipagePDFParserException(PDFParserException):
 
 
 class PDFParser(object):
-
     def __init__(self, target_dir: str):
         if not os.path.exists(target_dir):
             raise FileNotFoundError('Target directory {} doesnt exist'.format(target_dir))
@@ -70,5 +69,24 @@ class TetmlPDFParser(PDFParser):
 
 
 class OmnipagePDFParser(PDFParser):
-    pass
+    def __init__(self, omnipage_bin_path: str, target_dir: str):
+        if not os.path.exists(omnipage_bin_path):
+            raise OmnipagePDFParserException('{} doesnt exist'.format(omnipage_bin_path))
+        self.omnipage_bin_path = omnipage_bin_path
+
+        super(OmnipagePDFParser, self).__init__(target_dir)
+
+    def _parse(self, paper_id: str, source_pdf_path: str, target_path: str):
+        try:
+            cmd = '{bin} -i {input_path} -o {output_path}' \
+                .format(bin=self.omnipage_bin_path,
+                        input_path=source_pdf_path,
+                        output_path=target_path)
+            subprocess.run(cmd, shell=True, check=True)
+        except subprocess.CalledProcessError as e:
+            print(e)
+            raise OmnipagePDFParserException('OmniPage failed to parse {}'.format(source_pdf_path))
+
+    def get_target_path(self, paper_id: str) -> str:
+        return '{}.xml'.format(os.path.join(self.target_dir, paper_id))
 
