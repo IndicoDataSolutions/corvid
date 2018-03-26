19bffbc6aa405bcaf986886e4cb3c071735ad7a3
kyleclo <kyleclo@uw.edu>
2018-03-26 10:50:03 -0700

    script to parse pdfs to omnipage output

---
+++ scripts/parse_pdfs_to_omnipage.py
@@ -0,0 +1,53 @@
+"""
+
+Parse raw PDFs using Nuance's OmniPage
+
+"""
+
+import argparse
+import os
+
+from config import OMNIPAGE_BIN_PATH, PDF_DIR, OMNIPAGE_DIR
+from corvid.pipeline.pdf_parser import OmnipagePDFParser
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--parser', type=str,
+                        help='enter path to parser binary')
+    parser.add_argument('-i', '--input_dir', type=str,
+                        help='enter path to local directory containing pdfs')
+    parser.add_argument('-o', '--output_dir', type=str,
+                        help='enter path to local directory to save output tetml')
+    parser.add_argument('--overwrite', action='store_true',
+                        help='overwrite existing files')
+    args = parser.parse_args()
+
+    # define input files
+    pdf_dir = args.input_dir if args.input_dir else PDF_DIR
+    pdf_paths = [os.path.join(pdf_dir, path) for path in os.listdir(pdf_dir)]
+
+    # define output directory
+    output_dir = args.output_dir if args.output_dir else OMNIPAGE_DIR
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # define parser
+    pdf_parser = OmnipagePDFParser(omnipage_bin_path=OMNIPAGE_BIN_PATH,
+                                   target_dir=output_dir)
+
+    num_success = 0
+    for pdf_path in pdf_paths:
+        paper_id = os.path.basename(pdf_path).replace('.pdf', '')
+        try:
+            pdf_parser.parse(
+                paper_id=paper_id,
+                source_pdf_path=pdf_path
+            )
+            print('Parsed PDF {} to {}'.format(pdf_path, pdf_path))
+            num_success += 1
+        except Exception as e:
+            print(e)
+            print('Skipping PDF {}'.format(pdf_path))
+
+    print('Successfully parsed {}/{} pdfs.'.format(num_success,
+                                                   len(pdf_paths)))
