3a12aca378009aa38b8cba0731922060541951ea
kyleclo <kyleclo@uw.edu>
2018-04-04 23:46:08 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import setuptools
+
+setuptools.setup(
+    name='corvid',
+    version='0.0.1',
+    url='https://github.com/allenai/corvid',
+    packages=setuptools.find_packages()
+)
