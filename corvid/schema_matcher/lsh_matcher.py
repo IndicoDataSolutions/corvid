0e265f5531a004dd68e47170dcccb882da0d991b
cmkumar87 <cmkumar087@gmail.com>
2018-04-09 02:39:37 -0700

    hash the target schema using random projections method

--- corvid/schema_matcher/lsh_matcher.py
+++ corvid/schema_matcher/lsh_matcher.py
@@ -38,49 +38,41 @@ class LSHMatcher(SchemaMatcher):
             for idx_c, cell in enumerate(schema):
                 all_ngrams.append(self._make_char_ngrams(str(cell)))
 
-        ngram_term_weights = self._term_weigh_ngrams(all_ngrams)
+        ngram_string = [' '.join(ngrams) for ngrams in all_ngrams]
+        ngram_term_weights = self._term_weigh_ngrams(ngram_string)
 
-        # count = 0
-        # fname, url, records = next(
-        #     readline_google_store(ngram_len=1, indices=word[0]))
-        #
-        # try:
-        #     record = next(records)
-        #
-        #     while record.ngram != word:
-        #         record = next(records)
-        #
-        #     while record.ngram == word:
-        #         count = count + record.match_count
-        #         record = next(records)
-        #
-        # except StopIteration:
-        #     pass
-
+        # hash the target schema using random projections method
         for idx_c, cell in enumerate(target_schema[0, :]):
             ngram_vector = self._make_char_ngrams(str(cell))
-            count = Counter(ngram_vector)
-            ngram_count_vector = count.most_common(self.vector_dimension)
-            ngram_count_vector = [ngram_count_vector[i][1] for i in
-                                  range(len(ngram_count_vector))]
-            ngram_count_vector = self._pad_ngram_vector(ngram_count_vector, 100)
+            ngram_weight_vector = [ngram_term_weights[ngram] for ngram in
+                                   range(len(ngram_vector))]
+            ngram_weight_vector = self._pad_ngram_vector(ngram_weight_vector,
+                                                         100)
             self.matrix[idx_c, :] = nearpy.utils.utils.unitvec(
-                np.asanyarray(ngram_count_vector))
-            self.engine.store_vector(ngram_count_vector, idx_c)
+                np.asanyarray(ngram_weight_vector))
+            self.engine.store_vector(ngram_weight_vector, idx_c)
 
+        #query for the nearest neighbour to target schemas
         for idx_t, table in enumerate(tables):
             schema = table[0, 1:]
             column_mappings = []
             for idx_c, cell in enumerate(schema):
                 ngram_vector = self._make_char_ngrams(str(cell))
-                padded_ngram_vector = self._pad_ngram_vector(ngram_vector, 100)
+                ngram_weight_vector = [ngram_term_weights[ngram] for ngram in
+                                       range(len(ngram_vector))]
+                ngram_weight_vector = self._pad_ngram_vector(ngram_weight_vector,
+                                                            100)
                 # Get random query vector
                 # query = np.random.randn(self.dim)
                 print('\nNeighbour distances with RandomBinaryProjections:')
                 print('\n Candidate count is %d' % self.engine.candidate_count(
-                    padded_ngram_vector))
-                results = self.engine.neighbours(padded_ngram_vector)
-                print(results)
+                    ngram_weight_vector))
+                results = self.engine.neighbours(ngram_weight_vector)
+                print('  Data \t| Distance')
+                for r in results:
+                    data = r[1]
+                    dist = r[2]
+                    print('  {} \t| {:.4f}'.format(data, dist))
                 column_mappings.append((results[0], cell))
                 exit(0)
             pairwise_mappings.append(table1=target_schema,
@@ -93,18 +85,26 @@ class LSHMatcher(SchemaMatcher):
         return ngram_vector
 
     def _term_weigh_ngrams(self, ngram_vector) -> dict:
-        corpus = ["This is very strange",
-                  "This is very nice"]
         vectorizer = TfidfVectorizer(min_df=1)
-        X = vectorizer.fit_transform(corpus)
+        X = vectorizer.fit_transform(ngram_vector)
         idf = vectorizer.idf_
         return dict(zip(vectorizer.get_feature_names(), idf))
 
     def _make_char_ngrams(self, text: str) -> List:
         char_ngrams = []
-        char_ngrams.extend(list(ngrams(text, 1)))
-        char_ngrams.extend(list(ngrams(text, 2)))
-        char_ngrams.extend(list(ngrams(text, 3)))
+        # char_ngrams_tuples.extend(list(ngrams(text, 1)))
+        for tuple in (list(ngrams(text, 2))):
+            ngram = []
+            for element in tuple:
+                ngram.append(element)
+            char_ngrams.append(''.join(ngram))
+
+        for tuple in (list(ngrams(text, 3))):
+            ngram = []
+            for element in tuple:
+                ngram.append(element)
+            char_ngrams.append(''.join(ngram))
+
         return char_ngrams
 
 
