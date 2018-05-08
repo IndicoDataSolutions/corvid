38ba4cfe790a61f4abb547d661b012f389798c92
kyleclo <kyleclo@uw.edu>
2018-05-07 21:24:19 -0700

    bugfix table builder

--- corvid/semantic_table/table_builder.py
+++ corvid/semantic_table/table_builder.py
@@ -6,43 +6,48 @@ and Table as opposed to needing to override @classmethods
 
 """
 
-from typing import Callable, Dict
+from typing import Callable, Dict, List
 
 from corvid.semantic_table.table import Cell, Table
 
 
-class CellBuilder(object):
-    REQUIRED_KEYS = ['tokens', 'index_topleft_row', 'index_topleft_col',
-                     'rowspan', 'colspan']
+class CellBuilderException(KeyError):
+    pass
 
-    def __init__(self, cell_type: Callable[..., Cell]):
+
+class TableBuilderException(KeyError):
+    pass
+
+
+class CellBuilder(object):
+    def __init__(self,
+                 cell_type: Callable[..., Cell],
+                 required_keys: List[str] = None):
         self.cell_type = cell_type
+        self.required_keys = required_keys if required_keys else []
 
     def from_json(self, json: Dict) -> Cell:
-        kwargs = {k: v for k, v in json.items() if k not in self.REQUIRED_KEYS}
-        cell = self.cell_type(tokens=json['tokens'],
-                              index_topleft_row=json['index_topleft_row'],
-                              index_topleft_col=json['index_topleft_col'],
-                              rowspan=json['rowspan'],
-                              colspan=json['colspan'],
-                              **kwargs)
+        for k in self.required_keys:
+            if not json.get(k):
+                raise CellBuilderException('Missing key {}'.format(k))
+        cell = self.cell_type(**json)
         return cell
 
 
 class TableBuilder(object):
-    REQUIRED_KEYS = ['cells', 'nrow', 'ncol']
-
     def __init__(self,
                  table_type: Callable[..., Table],
-                 cell_builder: CellBuilder):
+                 cell_builder: CellBuilder,
+                 required_keys: List[str] = None):
         self.table_type = table_type
         self.cell_builder = cell_builder
+        self.required_keys = required_keys if required_keys else []
 
     def from_json(self, json: Dict) -> Table:
-        kwargs = {k: v for k, v in json.items() if k not in self.REQUIRED_KEYS}
+        for k in self.required_keys:
+            if not json.get(k):
+                raise TableBuilderException('Missing key {}'.format(k))
         cells = [self.cell_builder.from_json(d) for d in json['cells']]
-        table = self.table_type(cells=cells,
-                                nrow=json['nrow'],
-                                ncol=json['ncol'],
-                                **kwargs)
+        kwargs = {k: v for k, v in json.items() if k not in 'cells'}
+        table = self.table_type(cells=cells, **kwargs)
         return table
