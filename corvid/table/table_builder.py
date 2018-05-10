c949a238ec2a5658046850236c681f17cb7c5c79
kyleclo <kyleclo@uw.edu>
2018-05-10 12:15:29 -0700

    remove required keys arg from table builder since it was bugging out

--- corvid/table/table_builder.py
+++ corvid/table/table_builder.py
@@ -13,15 +13,10 @@ from corvid.table.table import Cell, Table
 
 class CellBuilder(object):
     def __init__(self,
-                 cell_type: Callable[..., Cell],
-                 required_keys: List[str] = None):
+                 cell_type: Callable[..., Cell]):
         self.cell_type = cell_type
-        self.required_keys = required_keys if required_keys else []
 
     def from_json(self, json: Dict) -> Cell:
-        for k in self.required_keys:
-            if not json.get(k):
-                raise KeyError('Missing key {}'.format(k))
         cell = self.cell_type(**json)
         return cell
 
@@ -29,16 +24,11 @@ class CellBuilder(object):
 class TableBuilder(object):
     def __init__(self,
                  table_type: Callable[..., Table],
-                 cell_builder: CellBuilder,
-                 required_keys: List[str] = None):
+                 cell_builder: CellBuilder):
         self.table_type = table_type
         self.cell_builder = cell_builder
-        self.required_keys = required_keys if required_keys else []
 
     def from_json(self, json: Dict) -> Table:
-        for k in self.required_keys:
-            if not json.get(k):
-                raise KeyError('Missing key {}'.format(k))
         cells = [self.cell_builder.from_json(d) for d in json['cells']]
         kwargs = {k: v for k, v in json.items() if k not in 'cells'}
         table = self.table_type(cells=cells, **kwargs)
