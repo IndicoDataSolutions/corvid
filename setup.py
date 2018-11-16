f054262899192a74f7fd6c20984ed7caa6c38a4e
Sergey Feldman <sergeyfeldman@gmail.com>
2018-11-16 10:17:39 -0800

    Update setup.py to include requirements
    We need these requirements to auto-install for the medical sex bias paper software release.

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
 
+import re
 import setuptools
+from os import path
+
+requirements_file = path.join(path.dirname(__file__), 'requirements.in')
+requirements = [r for r in open(requirements_file).read().split('\n') if not re.match(r'^\-', r)]
 
 setuptools.setup(
     name='corvid',
     version='0.0.1',
     url='https://github.com/allenai/corvid',
-    packages=setuptools.find_packages()
+    packages=setuptools.find_packages(),
+    install_requires=requirements # dependencies specified in requirements.in
 )
